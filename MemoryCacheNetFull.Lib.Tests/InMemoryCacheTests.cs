1966661865de075e251093512127f2effb7d64df
Aymen TROUDI <aymen.troudi@younited-credit.fr>
2019-04-20 00:44:37 +0200

    adding files

---
+++ MemoryCacheNetFull.Lib.Tests/InMemoryCacheTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MemoryCacheNetFull.Lib.Tests
+{
+    [TestClass]
+    public class InMemoryCacheTests
+    {
+        [TestMethod]
+        public async Task Get_Old_Value_When_Delay_Is_Not_Expired()
+        {
+            var cache = new InMemoryCache($"{DateTime.UtcNow}");
+
+            Assert.AreEqual(0, cache.Count());
+
+            var xx = await cache.AddOrGetAsync<int>("foo",
+                async () => await Task.FromResult(10));
+
+            Assert.AreEqual(1, cache.Count());
+            Assert.AreEqual(10, xx);
+
+            await Task.Delay(1000);
+
+            var yy = await cache.AddOrGetAsync<int>("foo",
+                async () => await Task.FromResult(20));
+
+            Assert.AreEqual(1, cache.Count());
+            Assert.AreEqual(10, yy);
+
+            Console.WriteLine(cache);
+        }
+
+        [TestMethod]
+        public async Task Get_New_Value_When_Delay_Is_Expired()
+        {
+            var cache = new InMemoryCache($"{DateTime.UtcNow}");
+
+            Assert.AreEqual(0, cache.Count());
+
+            var xx = await cache.AddOrGetAsync<int>("foo",
+                async () => await Task.FromResult(10));
+
+            Assert.AreEqual(1, cache.Count());
+            Assert.AreEqual(10, xx);
+
+            await Task.Delay(6000);
+
+            var yy = await cache.AddOrGetAsync<int>("foo",
+                async () => await Task.FromResult(20));
+
+            Assert.AreEqual(1, cache.Count());
+            Assert.AreEqual(20, yy);
+
+            Console.WriteLine(cache);
+        }
+    }
+}
---
+++ MemoryCacheNetFull.Lib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MemoryCacheNetFull.Lib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MemoryCacheNetFull.Lib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e5541b6-86b5-40d1-b33c-711261ffb9ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
